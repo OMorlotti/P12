56bd2f721c61fac6a0a379463b916c82fcaee1ff
Olivier Morlotti <o.morlotti@gmail.com>
2021-10-03 20:25:29 +0200

    Drive

---
+++ src/main/java/xyz/morlotti/lemur/controllers/SearchController.java
@@ -0,0 +1,24 @@
+package xyz.morlotti.lemur.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import xyz.morlotti.lemur.service.ArtistsService;
+import xyz.morlotti.lemur.service.ArtworksService;
+
+@Controller
+public class SearchController
+{
+	@Autowired
+	ArtistsService artistsService;
+
+	@Autowired
+	ArtworksService artworksService;
+
+	@RequestMapping(value = "/search", method = RequestMethod.GET)
+	public String artists()
+	{
+		return "search";
+	}
+}
