f17a2b0b7e4ef2cf6cd6536c84d5880307e7db8b
Olivier Morlotti <o.morlotti@gmail.com>
2021-10-03 20:26:47 +0200

    Drive

--- src/main/java/xyz/morlotti/lemur/controllers/ArtistsController.java
+++ src/main/java/xyz/morlotti/lemur/controllers/ArtistsController.java
@@ -1,9 +1,10 @@
 package xyz.morlotti.lemur.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import xyz.morlotti.lemur.service.ArtistsService;
 
 @Controller
