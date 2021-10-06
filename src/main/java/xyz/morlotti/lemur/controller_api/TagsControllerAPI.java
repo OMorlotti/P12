062f1f679c7d9d2b7108c432ec40b76ebdfacbf6
Olivier Morlotti <o.morlotti@gmail.com>
2021-10-06 18:39:08 +0200

    Formulaires

--- src/main/java/xyz/morlotti/lemur/controller_api/TagsControllerAPI.java
+++ src/main/java/xyz/morlotti/lemur/controller_api/TagsControllerAPI.java
@@ -1,13 +1,15 @@
 package xyz.morlotti.lemur.controller_api;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import xyz.morlotti.lemur.controller_api.bean.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import xyz.morlotti.lemur.model.bean.Tag;
 import xyz.morlotti.lemur.service.TagsService;
+import xyz.morlotti.lemur.controller_api.bean.DataSource;
 
 @RestController
 public class TagsControllerAPI
@@ -19,5 +21,13 @@ public class TagsControllerAPI
 	public DataSource<Tag> tags()
 	{
 		return new DataSource<Tag>(tagsService.getTags());
+	}
+
+	@RequestMapping(value = "/api/tags/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public String deleteTags(@PathVariable("id") int id)
+	{
+		System.out.println(id);
+
+		return "{}";
 	}
 }
