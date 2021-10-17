22259ef6af9e2d6cffffa5511e813667e0912449
Olivier Morlotti <o.morlotti@gmail.com>
2021-10-17 21:23:41 +0200

    remove Test.java

--- src/main/java/xyz/morlotti/lemur/security/WebSecurityConfig.java
+++ src/main/java/xyz/morlotti/lemur/security/WebSecurityConfig.java
@@ -1,18 +1,22 @@
 package xyz.morlotti.lemur.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import org.springframework.security.authentication.AuthenticationManager;
+
+import org.springframework.security.config.http.*;
+import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.config.annotation.method.configuration.*;
+import org.springframework.security.config.annotation.authentication.builders.*;
+
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 import xyz.morlotti.lemur.security.filter.AuthTokenFilter;
 import xyz.morlotti.lemur.security.service.UserDetailsServiceImpl;
 
